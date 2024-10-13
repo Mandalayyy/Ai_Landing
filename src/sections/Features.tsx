48d345ff5e93ee73b262b714e67663c3f728f6f8
  dimabielov <dimabielov@Noutbuk-dimabielov.local>
2024-10-13 17:22:53 +0300

    finish project

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,3 +1,11 @@
+'use client';
+
+import { DotLottieCommonPlayer, DotLottiePlayer } from "@dotlottie/react-player";
+import Image from "next/image";
+import productImage from "@/assets/product-image.png";
+import {animate, motion, useMotionTemplate, useMotionValue, ValueAnimationTransition} from 'framer-motion'
+import { ComponentProps, ComponentPropsWithoutRef, useEffect, useRef, useState } from "react";
+
 const tabs = [
   {
     icon: "/assets/lottie/vroom.lottie",
@@ -25,6 +33,126 @@ const tabs = [
   },
 ];
 
+const FeatureTab = (props: typeof tabs[number] & ComponentPropsWithoutRef<"div"> & {selected: boolean}) => {
+  const dotLottieRef = useRef<DotLottieCommonPlayer>(null);
+  const tabRef = useRef<HTMLDivElement>(null);
+  const handleTabHover = () => {
+    if (dotLottieRef.current === null) return;
+    dotLottieRef.current?.seek(0);
+    dotLottieRef.current?.play();
+  }
+
+  const xPersentage = useMotionValue(0);
+  const yPersentage = useMotionValue(0);
+
+  const maskImage = useMotionTemplate`radial-gradient(80px 80px at ${xPersentage}% ${yPersentage}%, white, black, transparent)`
+
+  useEffect(() => {
+    if (!tabRef.current || !props.selected) return;
+
+    xPersentage.set(0);
+    yPersentage.set(0);
+
+    const {width, height} = tabRef.current?.getBoundingClientRect();
+    const circumference = height * 2 + width * 2;
+
+
+    const times = [0,width / circumference, (width + height) / circumference, (width * 2 + height) / circumference ,1]
+    const options: ValueAnimationTransition = {
+      times,
+      duration: 4,
+      repeat: Infinity,
+      ease: 'linear',
+      repeatType: 'loop'
+    }
+
+    animate(xPersentage, [0, 100, 100, 0, 0], options)
+    animate(yPersentage, [0, 0, 100, 100, 0], options)
+  }, [props.selected]);
+
+  return (
+    <div 
+    onClick={props.onClick}
+    ref={tabRef}
+    onMouseEnter={handleTabHover} 
+    className="border border-white/15 flex p-2.5 rounded-lg gap-2.5 items-center lg:flex-1 relative"> 
+
+      {props.selected && (
+        <motion.div 
+        style={{
+          maskImage,
+        }}
+  
+        className="absolute inset-0 -m-px border border-[#a369ff] rounded-xl"></motion.div>
+      )}
+      
+
+      <div className="border h-12 w-12 border-white/15 rounded-lg inline-flex justify-center items-center">
+        <DotLottiePlayer ref={dotLottieRef} src={props.icon} className="h-5 w-5" autoplay/>
+      </div>
+      <div className="font-medium ">
+        {props.title}
+      </div>
+      {props.isNew && (
+        <div className="text-xs rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold">New</div>
+      )}
+    </div>
+  )
+}
+
 export const Features = () => {
-  return <section>Features</section>;
+
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  const backgroundPositionX = useMotionValue(tabs[0].backgroundPositionX);
+  const backgroundPositionY = useMotionValue(tabs[0].backgroundPositionY);
+  const backgroundSizeX = useMotionValue(tabs[0].backgroundSizeX);
+
+  const backgroundSize = useMotionTemplate`${backgroundSizeX}% auto`;
+  const backgroundPosition = useMotionTemplate`${backgroundPositionX}% ${backgroundPositionY}%`;
+
+  const handleSelectedTab = (index: number) => {
+    setSelectedTab(index);
+
+    const animateOprions: ValueAnimationTransition = {
+      duration: 2,
+      ease: 'easeInOut',
+    }
+
+    animate(backgroundSizeX, [backgroundSizeX.get(),100,tabs[index].backgroundSizeX,], animateOprions);
+
+    animate(backgroundPositionX, [backgroundPositionX.get(),tabs[index].backgroundPositionX,], animateOprions)
+
+    animate(backgroundPositionY, [backgroundPositionY.get(),tabs[index].backgroundPositionY,], animateOprions)
+  }
+
+  return (
+  <section className="py-20 md:py-24">
+    <div className="container">
+      <h2 className="section-title">Elevate your SEO efforts.</h2>
+      <p className="section-description max-w-2xl mt-5">From small startups to large enterprises, our AI-driven tool has revolutionized the way businesses approach SEO.</p>
+      <div className="mt-10 flex flex-col lg:flex-row gap-3">
+        {tabs.map((tab, index) => (
+          <FeatureTab
+          selected={selectedTab === index}
+          key={index} 
+          onClick={() => handleSelectedTab(index)} 
+          {...tab} />
+        ))}
+      </div>
+      <div className="border border-white/20 p-2.5 rounded-xl mt-3">
+        <motion.div 
+
+        
+        className="aspect-video bg-cover border border-white/20 rounded-lg" 
+        
+        style={{
+          backgroundPosition,
+          backgroundSize,
+          backgroundImage: `url(${productImage.src})`,
+        }}></motion.div>
+      </div>
+     
+    </div>
+  </section>);
 };
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import avatar1 from "@/assets/avatar-1.png";
 import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
 import avatar4 from "@/assets/avatar-4.png";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
 const testimonials = [
   {
@@ -31,5 +35,45 @@ const testimonials = [
 ];
 
 export const Testimonials = () => {
-  return <section>Testimonials</section>;
+  return (
+  <section className="py-20 md:py-24">
+    <div className="container">
+      <h2 className="section-title">Beyond Expectations.</h2>
+      <p className="section-description max-w-sm mx-auto mt-5">Our revolutionary AI SEO tools have transformed our clients' strategies.</p>
+      <div className="flex overflow-hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+        <motion.div 
+        initial = {{
+          translateX: "-50%"
+        }}
+        animate={{
+          translateX: "0%"
+        }}
+        transition={{
+          repeat: Infinity,
+          duration: 30,
+          ease: 'linear',
+        }}
+        className="flex gap-5 pr-5 flex-none">
+          {[...testimonials, ...testimonials].map(({text, name, title, avatarImg}, index) => (
+            <div className="border border-white/15 p-6 md:p-10 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-md flex-none" key={index}>
+              <div className="text-lg md:text-2xl tracking-tight ">{text}</div>
+              
+              <div className="flex items-center gap-3 mt-5">
+                <div className="relative avatar-overlay">
+                  <Image src={avatarImg} alt={name} className="h-11 w-11 rounded-lg grayscale " width={avatarImg.width} height={avatarImg.height} />
+                </div>
+                
+                <div className="">
+                  <div >{name}</div>
+                  <div className="text-white/50 text-sm ">{title}</div>
+                </div>
+              
+              </div>
+            
+            </div>
+          ))}
+        </motion.div>
+      </div>
+    </div>
+  </section>);
 };
